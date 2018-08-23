1ed03ccf50949a010798609cfacd4eb7996504b7
johnfaben <jdfaben@gmail.com>
2018-08-23 21:05:31 +0100

    Finished Higher or Lower answers

--- HigherOrLower/higher_or_lower.py
+++ HigherOrLower/higher_or_lower.py
@@ -7,6 +7,9 @@ Created on Thu Aug 23 18:28:46 2018
 """
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import itertools
 
 
 def higher_or_lower(n=10):
@@ -14,6 +17,7 @@ def higher_or_lower(n=10):
     np.random.shuffle(order)
     deck = [1 for i in range(n)]
     current = -1
+    lower = False
     for card in order:
         #print(card)
         if current > -1:
@@ -29,11 +33,32 @@ def higher_or_lower(n=10):
             lower = False
     return 1
         
-
-for k in range(1,21):
+win_probs = [1]
+for k in range(1,25):
     n = 100000    
     wins = 0.0
     for i in range(n):    
         wins += higher_or_lower(k)
+    win_probs.append(wins/n)
 
-    print(k,wins/n)
+
+powers = [1,1] + [0.75**n for n in range(25)]
+
+plt.plot(powers, label = '0.75^n-2')
+plt.plot(win_probs, label = 'win probability')
+
+plt.annotate('(%d,%.3f)' %(4,win_probs[4]),xy=(4,win_probs[4]))
+plt.annotate('(%d,%.3f)' %(9,win_probs[9]),xy=(9,win_probs[9]))
+plt.annotate('(%d,%.3f)' %(20,win_probs[20]),xy=(20,win_probs[20]))
+plt.legend()
+plt.xticks(range(0,25,4))
+plt.show()
+    
+
+
+
+    
+    
+    
+
+    
